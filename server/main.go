c97ceb5bf9e5325c801c52bb3455fb23131373fc
sadeghbiglar <sbmail555@gmail.com>
2025-01-28 17:34:01 +0330

    add air

--- server/main.go
+++ server/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main()  {
+	"github.com/gin-gonic/gin"
+	"github.com/sadeghbiglar/bookshop/config"
+)
+
+func main() {
+	app := gin.Default()
+	app.Use(gin.Recovery())
+	app.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message":    "server is running fine!",
+			"statusCode": 200,
+		})
+	})
 	fmt.Println("Go app started successfully !")
-	
+	port := config.GetEnvProperty("port")
+	fmt.Println(port)
+	app.Run(port)
 }
